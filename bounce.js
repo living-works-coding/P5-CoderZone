a65917e232cd53c54d8fc73be80780d0957d0056
Joshua Alletto <joshalletto@gmail.com>
2018-02-09 20:37:43 -0600

    add var to variable declarations.

--- bounce.js
+++ bounce.js
@@ -1,21 +1,19 @@
-x = 175;
-y = 250;
-bounceVariance = 0;
-goingUp = true;
+var x = 175;
+var y = 250;
+var bounceVariance = 0;
+var goingUp = true;
 
 // runs once
 function setup() {
   createCanvas(500, 500);
-  background(0);
 }
 
 // draw is an infinite loop
 function draw() {
   background(0);
 
-  fill(255);
-  textSize(32);
-  word = 'Bounce';
+  fill(255, 45, 57);
+
 
   if (bounceVariance <= 25 && goingUp) {
     bounceVariance += 1;
@@ -29,13 +27,11 @@ function draw() {
     bounceVariance -= 1;
   }
 
-  i = 0;
-  while (i < word.length) {
+  var i = 0;
+  while (i < 5) {
     if (i % 2 === 1) {
-      // text(word[i], x + i * 32, y + bounceVariance);
       ellipse(x + i * 25, y + bounceVariance, 25, 25);
     } else {
-      // text(word[i], x + i * 32, y - bounceVariance);
       ellipse(x + i * 25, y - bounceVariance, 25, 25);
     }
     i = i + 1;
